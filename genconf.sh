4c8d33812e36b7f2caab604ed4b4b214cb460f90
phantomic12 <76127596+phantomic12@users.noreply.github.com>
2022-11-25 12:25:25 -0500

    lets see what happens

--- genconf.sh
+++
@@ -1,7 +0,0 @@
-sudo build-pacman-repo print-config \
-  --repository $repo_dir/$repo_name.db.tar.gz \
-  --container pkg \
-  --require-pkgbuild \
-  --require-srcinfo \
-  --with-install-missing-dependencies true \
-  > build-pacman-repo.yaml
