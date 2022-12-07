a35430d5b5ad3ba32df345b220fecb6b1bf90dca
phantomic12 <yoavtirosh54@gmail.com>
2022-12-07 03:13:47 -0500

    add folders and stuff

---
+++ bimonthly-git/repo.sh
@@ -0,0 +1,10 @@
+mkdir -p pacman-repo/repo
+cd pacman-repo
+cd repo
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db.tar.gz
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db.tar.gz.old
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files.tar.gz
+wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files.tar.gz.old
+cd .. && cd ..
