629cd50c4a891cd69a8424cf2e69a882c4f658b5
phantomic12 <yoavtirosh54@gmail.com>
2022-12-07 19:59:44 -0500

    add new workflows
    temporarily being kept invalid until package lists are made

--- hourly-bin/repo.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p pacman-repo/repo
-cd pacman-repo/repo
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db.tar.gz
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.db.tar.gz.old
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files.tar.gz
-wget http://downloads.sourceforge.net/phn-repo/repo/phn-repo.files.tar.gz.old
-cd .. && cd ..
