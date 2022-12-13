712b477d3a344b6cf305f3c65d402bb775b39b79
phantomic12 <yoavtirosh54@gmail.com>
2022-12-13 15:54:31 -0500

    oops

---
+++ bimonthly-git/c.sh
@@ -0,0 +1,3 @@
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
