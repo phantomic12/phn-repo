6d1eb870b508e70d1366faa9f109ffe9e4cbbdfc
phantomic12 <yoavtirosh54@gmail.com>
2022-12-14 19:53:59 -0500

    remove firedragon

--- bimonthly-git/c.sh
+++
@@ -1,4 +0,0 @@
-pacman-key --init
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
